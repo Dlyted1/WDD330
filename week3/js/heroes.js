68f1ee6b1ade720eff4d7490dd211dc2682939bb
Dlyted1 <mookathy3@gmail.com>
2022-05-04 10:32:52 -0400

    commit

--- week3/js/heroes.js
+++ week3/js/heroes.js
@@ -1,8 +1,6 @@
-const aquaman = createElement('li','Aquaman');
-heroes.appendChild(flash);
-
 const heroes = document.getElementById('roster');
 const superman = heroes.children[0];
 
 superman.style.border = "red 2px solid";
+superman.style['background color'] = 'blue';
 
