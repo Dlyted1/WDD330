a8e9b44caedc2dfd5d8bd3570534cfde494beaa9
Dlyted1 <mookathy3@gmail.com>
2022-05-05 19:13:49 -0400

    commit

--- week3/js/events.js
+++ week3/js/events.js
@@ -1,5 +1,8 @@
-function doSomething(){
-  console.log('Something Happened!');
-}
+// function doSomething(){
+//  console.log('Something Happened!');
+//  }
 
+function doSomething(event){
+  console.log(event.type);
+}
 addEventListener('click', doSomething);
