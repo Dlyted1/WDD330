c57d13b03bc3c767c2c2e5058e1449bcb1608471
Dlyted1 <mookathy3@gmail.com>
2022-05-05 21:34:45 -0400

    commit

--- week3/js/quizNinja3.js
+++ week3/js/quizNinja3.js
@@ -11,12 +11,20 @@ const view = {
     question: document.getElementById('question'),
     result: document.getElementById('result'),
     info: document.getElementById('info'),
+    start: document.getElementById('start'),
     render(target,content,attributes) {
         for(const key in attributes) {
             target.setAttribute(key, attributes[key]);
         }
         target.innerHTML = content;
+    },
+    show(element){
+        element.style.display = 'block';
+    },
+    hide(element){
+        element.style.display = 'none';
     }
+
 };
 
 // placing them inside an object called game that will be the namespace. This means that any references to the functions need to be replaced with game.function() outside the object or this.function() inside the object. //
@@ -25,7 +33,7 @@ const game = {
   start(quiz){
       this.score = 0;
       this.questions = [...quiz];
-      
+      view.hide(view.start);
       // main game loop
       for(const question of this.questions){
       this.question = question;
@@ -54,9 +62,12 @@ const game = {
   },
   gameOver(){
     view.render(view.info,`Game Over, you scored ${this.score} point${this.score !== 1 ? 's' : ''}`);
+    view.show(view.start);
   }
 }
      
 
 // After this, we have to edit the function that starts the game, so it includes the namespace: //
-game.start(quiz);
+// game.start(quiz);
+
+view.start.addEventListener('click', () => game.start(quiz), false);
