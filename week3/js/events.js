051019cc036827b15e99386319455a214278e77e
Dlyted1 <mookathy3@gmail.com>
2022-05-05 20:02:40 -0400

    commit

--- week3/js/events.js
+++ week3/js/events.js
@@ -1,12 +1,21 @@
-function doSomething(){
-console.log('Something Happened!');
+const clickParagraph = document.getElementById('click');
+
+clickParagraph.addEventListener('click',() => console.log('click') );
+clickParagraph.addEventListener('mousedown',() => console.log('down') );
+clickParagraph.addEventListener('mouseup',() => console.log('up') );
+
+const dblclickParagraph = document.getElementById('dblclick');
+dblclickParagraph.addEventListener('dblclick', highlight);
+
+function highlight(event){
+    event.target.classList.toggle('highlight');
 }
+addEventListener('keydown',highlight);
+const onceParagraph = document.getElementById('once');
+onceParagraph.addEventListener('click', remove);
 
-// function doSomething(event){
-//  console.log(event.type);
-// } 
-addEventListener('click', doSomething);
-
-const mouseParagraph = document.getElementById('mouse');
-mouseParagraph.addEventListener('mouseover', highlight);
-mouseParagraph.addEventListener('mouseout', highlight);
+function remove(event) {
+    console.log('Enjoy this while it lasts!');
+    onceParagraph.style.backgroundColor = 'pink';
+    onceParagraph.removeEventListener('click',remove);
+}
