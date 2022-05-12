f59da7b84e1faa9b9d59c7f8a2218ae15666abe4
Kathy Moore <mookathy3@gmail.com>
2022-05-12 14:27:00 -0400

    commit

---
+++ week4/js/quizninja4Oop.js
@@ -0,0 +1,96 @@
+// Set the questions
+const quiz = [
+    { name: "Superman",realName: "Clark Kent" },
+    { name: "Wonder Woman",realName: "Diana Prince" },
+    { name: "Batman",realName: "Bruce Wayne" },
+];
+
+// View Object
+const view = {
+    score: document.querySelector('#score strong'),
+    question: document.getElementById('question'),
+    result: document.getElementById('result'),
+    info: document.getElementById('info'),
+    start: document.getElementById('start'),
+   // add below a reference to the form in our JavaScript. Add the following line of code as a property of the View Object
+    response: document.querySelector('#response'),
+    
+    render(target,content,attributes) {
+        for(const key in attributes) {
+            target.setAttribute(key, attributes[key]);
+        }
+        target.innerHTML = content;
+    },
+    show(element){
+        element.style.display = 'block';
+    },
+    hide(element){
+        element.style.display = 'none';
+    },
+    resetForm(){
+        this.response.answer.value = '';
+        this.response.answer.focus();
+    },
+    setup(){
+        this.show(this.question);
+        this.show(this.response);
+        this.show(this.result);
+        this.hide(this.start);
+        this.render(this.score,game.score);
+        this.render(this.result,'');
+        this.render(this.info,'');
+        this.resetForm();
+    },
+    teardown(){
+        this.hide(this.question);
+        this.hide(this.response);
+        this.show(this.start);
+    }   
+};
+
+// placing them inside an object called game that will be the namespace. This means that any references to the functions need to be replaced with game.function() outside the object or this.function() inside the object. //
+// Game Object
+const game = {
+    start(quiz){
+        this.score = 0;
+        this.questions = [...quiz];
+        view.setup();
+        this.ask();
+  },
+  ask(name){
+    if(this.questions.length > 0) {
+        this.question = this.questions.pop();
+        const question = `What is ${this.question.name}'s real name?`;
+        view.render(view.question,question);
+    }
+    else {
+        this.gameOver();
+    }
+  },
+  check(event){
+    event.preventDefault();
+    const response = view.response.answer.value;
+    const answer = this.question.realName;
+    if(response === answer){
+        view.render(view.result,'Correct!',{'class':'correct'});
+        this.score++;
+        view.render(view.score,this.score);
+    } else {
+        view.render(view.result,`Wrong! The correct answer was ${answer}`,{'class':'wrong'});
+    }
+    view.resetForm();
+    this.ask();
+  },
+  gameOver(){
+    view.render(view.info,`Game Over, you scored ${this.score} point${this.score !== 1 ? 's' : ''}`);
+    view.teardown();
+  }
+}
+     
+
+// After this, we have to edit the function that starts the game, so it includes the namespace: //
+// game.start(quiz);
+
+view.start.addEventListener('click', () => game.start(quiz), false);
+view.response.addEventListener('submit', (event) => game.check(event), false);
+view.hide(view.response);
