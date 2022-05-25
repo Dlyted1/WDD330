f3967633a1d1a9ddcae5928d747bf5ae51a5fc0f
Kathy Moore <mookathy3@gmail.com>
2022-05-25 12:41:56 -0400

    commit

---
+++ todo/js/todos.js
@@ -0,0 +1,117 @@
+import utils from './utils.js';
+import ls from './ls.js';
+
+// load the list
+loadTodos();
+
+// add event listeners with...
+//On click handler to button
+document.querySelector("#addBtn").onclick = addNewTodo;
+// Get Input
+const input = document.querySelector('#todoInput');
+//add on Enter
+input.addEventListener('keypress', e => {
+    if (e.keycode == '13') addNewTodo();
+})
+
+
+// step 1
+function addNewTodo(e) {
+     const todo = { id: Date.now(), content: input.ariaValueMax, completed: false };
+     // reset the input field
+     input.value = '';
+     
+     // Add to the UI
+     const todoItem = createTodoItem(todo);
+
+     //Save to localStorage ls
+     ls.saveTodo(todo);
+
+     loadTodos();   
+}
+
+// step 2 ? function createTodo {}
+function createTodoItem(todo) {
+    //todo div
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+
+    //complete button
+    const completeBtn = document.createElement('button');
+    completeBtn.setAttribute('data-id', todo.id);
+    completeBtn.classList.add('complete-btn');
+
+    //todo content
+    const todoContent = document.createElement('div');
+    todoContent.innerText = todo.content;
+    todoContent.classList.add('todo-content');
+
+    //delete btn
+    const deleteBtn = document.createElement('button');
+    deleteBtn.setAttribute('data-id', todo.id);
+    deleteBtn.classList.add('todo-delete-btn');
+    deleteBtn.innerText = "X";
+    deleteBtn.onclick = deleteTodo;
+
+    todoDiv.appendChild(completeBtn);
+    todoDiv.appendChild(todoContent);
+    todoDiv.appendChild(deleteBtn);
+
+    return todoDiv;
+
+}
+
+function addToList(todoDiv) {
+    // Add to the document
+    document.querySelector('#todos').appendChild(todoDiv);
+
+}
+// step 0
+function loadTodos() {
+    //document.querySelector('#todos').innerHTML = '';
+    const todoList = ls.getTodoList();
+    // debugging
+    // console.log(todoList)
+
+    todoList.forEach(todo => {
+        const el = createTodoElement(todo)
+        addToList(el);
+    })
+}
+
+//events
+function deleteTodo(e) {
+    const btn = e.currentTarget;
+    ls.deleteTodo(btn.getAttribute('data-id'));
+    document.querySelector('#todos').innerHTML = '';
+    loadTodos();
+
+}
+
+function toggleComplete(e) {
+
+}
+
+function applyFilter(e) {
+    // clear the list
+    document.querySelector('#todos').innerHTML = '';
+
+    // declare variables
+    let filterdTodos = [];
+    const allTodos = ls.getTodoList();
+
+    // check which filter to apply
+    if (e.currentTarget.id == 'activefilter') {
+        filteredTodos = utils.activeFilter(allTodos)
+
+    }
+
+
+    // draw the list
+    filteredTodos.forEach( todo => {
+        const el = createTodoElement(todo)
+        addToList(el)
+    })
+
+
+}
