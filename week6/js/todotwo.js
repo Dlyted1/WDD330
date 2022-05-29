bc71643895daaa93e7a1ccaf3720553243a412ab
Kathy Moore <mookathy3@gmail.com>
2022-05-28 23:50:34 -0400

    commit

--- week6/js/todotwo.js
+++ week6/js/todotwo.js
@@ -1,93 +1,140 @@
+// select the elements
 const clear = document.querySelector(".clear");
-
-
-
 const dateElement = document.getElementById("date");
-
-
-
-
-
 const list = document.getElementById("list");
-list.addEventListener("click", function(event){
-    let element = event.target;
-    const elementJOB = event.target.attributes.job.value;
-    if(elementJOB == "complete"){
-        completeToDo(element);
-    }else if (elementJOB == "delete"){
-        removeToDo(element);
-    }
-});
+const input = document.getElementById("input");
+const addButton = document.querySelector('.fas fa-angle-right');
+
+// Class names
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle-thin";
+const LINE_THROUGH = "lineThrough";
+
+//Variables
+let LIST, id;
+// let LIST = [], id = 0;
+
+//get item from local storage
+let data = localStorage.getItem("TODO");
 
 
-const CHECK ="fa-check-circle";
-const UNCHECK "fa-circle-thin";
-const LINE_THROUGH ="lineThrough";
-
-function addToDo(toDo ,id,done , trash){
-    if(trash){ return; }
-
-    const DONE = done ? CHECK : UNCHECK;
-    const LINE = done ? LINE_THROUGH: "";
-
-    const text = `<li class="item">
-        <i class="fa ${DONE} complete" job="complete" id="${id}"></i>
-        <p class="text ${LINE}"> ${toDo} </p>
-        <i class="fa fa-trash-o delete" job="delete" id="${id}"></i>
-
-    </li>`
-
-    const position = "beforeend";
-    list.insertAdjacentHTML(position, text);
+//check if data not empty
+if(data) {
+    LIST = JSON.parse(data);
+    id = LIST.length; //set id to last one in list
+    loadList(LIST); //loads list to user interface
+}
+else{
+    LIST = [];
+    id = 0;
 }
 
-addToDo("Feed Horses");
-
-
-
-const input = document.getElementById("input");
-
-document.addEventListener("keyup", function(event) {
-    if(event.keyCode == 13){
-        const toDo = input.value;
-            if(todo){
-                addToDo(toDo,id, false, false);
-                list.push(
-                    {
-                        name: toDo,
-                        id: id,
-                        done: false,
-                        trash: false
-                    }
-            );
-            input.value = "";
-            id++;
-
-        }
-    }
-
+//load items to users interface
+function loadList(array) {
+    array.forEach(element => {
+        add_to_do(element.name, element.id, element.done, element.trash);
+    });
+}
+// clear local storage
+clear.addEventListener("click", function() {
+    localStorage.clear();
+    location.reload;
 });
 
-//element.classList.toggle(CLASS) {
-//    if ("CLASS exists, remove CLASS");
- //   } else {
-//        CLASS !exists, add CLASS
-// }
-function completeToDo(element) {
+
+// add to local storage
+//below line needs to be added everywhere the LIST array is updated(copy & paste code)
+localStorage.setItem("TODO", JSON.stringify(LIST));
+
+
+//Show date
+const options = {weekday: 'long', month: 'short', day: 'numeric'};
+const today = new Date();
+
+dateElement.innerHTML = today.toLocaleDateString("en-US", options);
+
+//to do function
+function add_to_do(toDo, id, done, trash) {
+    if (trash)
+        return;
+    // if(trash){ return; }
+
+    const DONE = done ? CHECK : UNCHECK;
+    const LINE = done ? LINE_THROUGH : "";
+
+    const item = `<li class="item">
+    <i class="fa ${DONE} co" job="complete" id="${id}"></i>
+    <input class="text ${LINE}" job="edit" value=${toDo}id="${id}" Blur=""">
+    <i class="fa-solid fa trash" job="delete" id="${id}"></i>
+    </li>
+    `;
+
+    const position = "beforeend";
+
+    list.insertAdjacentHTML(position, item);
+}
+
+//Add item to list
+input.addEventListener("keypress",function(even) {
+    if(event.keyCode === 13){
+    const toDo = input.value; 
+
+//Add Button Click
+//const addItem = document.getElementById("add-btn");
+//addItem.addEventListener("click", function(even){
+    //console.log("addItem button clicked")
+    //const toDo = input.value;
+
+        // when input isn't empty
+        if(toDo) {
+            add_to_do(toDo, id, false, false);
+
+            LIST.push({
+                name: toDo,
+                id : id,
+                done: false,
+                trash : false
+            });
+            //below line needs to be added everywhere the LIST array is updated(copy & paste code)
+            localStorage.setItem("TODO", JSON.stringify(LIST));
+
+            id++;
+            input.value = ""; 
+        }       
+
+    }
+});
+
+//complete todo and toggle
+function completeToDo(element){
     element.classList.toggle(CHECK);
     element.classList.toggle(UNCHECK);
     element.parentNode.querySelector(".text").classList.toggle(LINE_THROUGH);
-         LIST[element.id].done = LIST[element.id].done ? false : true;
 
+    LIST[element.id].done = LIST[element.id].done ? false : true;
 }
 
-                        LIST[0]: { item: "Feed Horses",
-                                   id : 0
-                                   done : false,
-                                   trash : false
-                                 }
-
-function removeToDo (element) {
+// remove todo
+function removeToDo(element){
     element.parentNode.parentNode.removeChild(element.parentNode);
+
     LIST[element.id].trash = true;
 }
+
+//eventlistener to target items created
+list.addEventListener("click", function(event) {
+    const element = event.target; //return clicked element inside list
+    const elementJob = element.attributes.job.value; // complete or delete
+
+    if(elementJob === "complete") {
+        completeToDo(element);
+    } else if (elementJob === "delete") {
+        removeToDo(element);
+    }
+    //below line needs to be added everywhere the LIST array is updated(copy & paste code)
+    localStorage.setItem("TODO", JSON.stringify(LIST));
+});
+
+
+
+
