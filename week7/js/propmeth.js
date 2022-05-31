56b2217267b377247d3b7f1d91cc04627ed6f51a
Kathy Moore <mookathy3@gmail.com>
2022-05-31 12:05:10 -0400

    commit

--- week7/js/propmeth.js
+++ week7/js/propmeth.js
@@ -1,15 +1,35 @@
+// Property Variables
 var item = "Bottle of water";
 console.log(item.length);   //Example of a property but use the  .length behind the item. to calculate the length of the specific string "Bottle of water" will be 15
 
+//Methods
 var item = "Can of soda";
 console.log(item.indexOf("of")); // Method example put behind item. the 1st work not capitalized but the secondword will be camelcase - looking for precision of the word of
 
-// substring example
+// method substring example
 //to cut off a part of the string
 var item = "Glass of water";
-console.log(item.substring(0, 6))
+console.log(item.substring(0, 6)) // first number where to start second number where to cut off in the string
+
+//method replace example
+var item = "Glass of milk";
+console.log(item.replace("milk", "juice"));
+
+//method UPPERCASE
+var item = "Can of Juice";
+console.log(item.toUpperCase());
+
+//Array usage
 
 
+var items = ["Bottle", 4, true];  //item is now plural for array
+console.log(items);  //to see what is in the array -click arrow in console to see differ properties, methods and index that we can use. on last line we can extend further by clicking on arrow in console see the differ methods we can use in the array
+
+// Join in array
+var items = ["Can, 5, true"];
+console.log(items.join("| &")); // can use any character to join
+// console.log(items.join(" - "));
+// console.log(items.join(", "))
 
 
 
