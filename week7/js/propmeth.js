5f422ee5956bcafe9a1cae944d8d89a22a1e3c05
Kathy Moore <mookathy3@gmail.com>
2022-05-31 11:34:12 -0400

    commit

--- week7/js/propmeth.js
+++ week7/js/propmeth.js
@@ -1,2 +1,8 @@
 var item = "Bottle of water";
-console.log(item.length);   //the method is behind the item. here to calculate the length of the specific string "Bottle of water"
+console.log(item.length);   //Example of a property but use the  .length behind the item. to calculate the length of the specific string "Bottle of water" will be 15
+
+var item = "Can of soda";
+console.log(item.indexOf("of")); // Method example put behind item. the 1st work not capitalized but the secondword will be camelcase - looking for precision of the word of
+
+
+
