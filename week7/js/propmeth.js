8b1108e0897af0d50305e133c80c0b60e0d0e917
Kathy Moore <mookathy3@gmail.com>
2022-05-31 12:27:05 -0400

    commit

--- week7/js/propmeth.js
+++ week7/js/propmeth.js
@@ -26,11 +26,27 @@ var items = ["Bottle", 4, true];  //item is now plural for array
 console.log(items);  //to see what is in the array -click arrow in console to see differ properties, methods and index that we can use. on last line we can extend further by clicking on arrow in console see the differ methods we can use in the array
 
 // Join in array
-var items = ["Can, 5, true"];
-console.log(items.join("| &")); // can use any character to join
+var items = ["Can", 5, true];
+console.log(items.join(" | & ")); // can use any character to join
 // console.log(items.join(" - "));
 // console.log(items.join(", "))
 
 
+//Pop - removes an item inside array console.log(items.pop());
+var items = ["Bottle", 6, true];
+//console.log(items.pop());  to see what item is being removed
+items.pop(); //will remove the last item from array
+console.log(items);
+
+//Pop and join
+var items = ["Bottle", 4, true];
+items.pop();
+console.log(items.join(", "));
+
+//Push will add item instead of removing
+var items = ["Bottle", 3, true];
+items.push("Juice");  // to add item juice to array
+console.log(items);
+// console.log(items.push("Juice")); // to show where its; now indexed
 
 
