b2b98c9d664ed821378f46f629b6fc7c4fa5e123
Kathy Moore <mookathy3@gmail.com>
2022-05-31 13:59:44 -0400

    commit

--- week7/js/further.js
+++ week7/js/further.js
@@ -1,5 +1,9 @@
 // Immediately Invoked Function Expression or IIFE
 
 (function() {
-    console.log("My favorite number is 7");
-    })();
+    console.log("My favorite number is 3"); // first ()'s make it an expression
+    })();  // last two () invokes or calls the function immediately
+
+    (favNumber = function(num = 3) {
+        console.log("My favorite number is " + num); 
+        })();
