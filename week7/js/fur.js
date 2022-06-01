3e2c872d266fad53c8c355d8f0c21a046aa08a1d
Kathy Moore <mookathy3@gmail.com>
2022-06-01 13:10:44 -0400

    commit

--- week7/js/fur.js
+++ week7/js/fur.js
@@ -163,3 +163,43 @@ frenchGreeter();
 const germanGreeter = greeter('Guten Tag');
 germanGreeter();
 //Guten Tag
+
+//CLOSURES
+   //A function can access all variables defined inside the function and a is a local variable like this:
+myFunction();
+
+function myFunction() {
+  let a = 4;
+  document.getElementById("demo").innerHTML = a * a;
+}
+
+  //But a function can also access variables defined outside the function, here the a is a global variable, like this:
+let a = 4;
+myFunction();
+
+function myFunction() {
+  document.getElementById("demo").innerHTML = a * a;
+} 
+
+//Closure Example
+const add = (function () {
+    let counter = 0;
+    return function () {counter += 1; return counter;}
+  })();
+  
+  function myFunction(){
+    document.getElementById("demo").innerHTML = add();
+  }
+
+      //Above Example Explained: The variable add is assigned to the return value of a self-invoking function.
+              //The self-invoking function only runs once. It sets the counter to zero (0), and ////returns a function expression.
+              //This way add becomes a function. The "wonderful" part is that it can access the counter in the parent scope.
+              //This is called a JavaScript closure. It makes it possible for a function to have "private" variables.
+              //The counter is protected by the scope of the anonymous function, and can only be changed using the add function.
+
+function reverse(string) {
+    return string.split('').reverse().join('');
+}
+
+const message = 'Hello JavaScript';
+reverse(message);
