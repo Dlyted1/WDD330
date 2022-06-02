9dca8f5b8bdf8cc6b4ed032d0435c99f4d2c6d54
Kathy Moore <mookathy3@gmail.com>
2022-06-02 19:32:53 -0400

    commit

--- week7/js/ajax.js
+++ week7/js/ajax.js
@@ -7,11 +7,31 @@ window.onload = function() {
     var xhr = new XMLHttpRequest();
         getText.addEventListener("click",function() {
             xhr.addEventListener('readystatechange',function(){
-                content.innerHTML = this.responseText;
+                content.innerHTML = this.responseText + '<br />';
              });
              xhr.open("GET","hello.txt",true);
              xhr.send();
         });
 
     
+
+
+//grabbing JSON
+var getJSON = document.getElementById("getJSON");
+var j = new XMLHttpRequest();
+getJSON.addEventListener("click",function() {
+
+    j.addEventListener("readystatechange",function() {
+        var data = JSON.parse(j.responseText);
+
+        for(p in data) {
+            content.innerHTML += p + ": " + data[p] + '<br />';
+        }
+
+    });
+
+    j.open("GET","ajax.JSON",true);
+    j.send();
+});
+
 };
