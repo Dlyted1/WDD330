140da8dc8afb6f2399ca0a0b3f3653bb820caa05
Kathy Moore <mookathy3@gmail.com>
2022-06-04 23:59:55 -0400

    commit

--- week7/js/comments.js
+++ week7/js/comments.js
@@ -5,7 +5,7 @@ class CommentType {
         this.comments=readFromLS(this.type) || [];
     }
 
-    getComments(q === null) {
+    getComments(q = null) {
         if (q === null) {
             return this.comments;
         } else {
@@ -59,3 +59,52 @@ function renderCommentList(element, comments) {
 
 
 // Comments: this code handles getting the list of comments from the data source, and outputting them to the screen at the right time.
+class Comments {
+    constructor(type, commentElementId) {
+      this.type = type;
+      this.commentElementId = commentElementId;
+      this.model = new CommentModel(this.type);
+    }
+  
+    addSubmitListener(postName) {
+      // use element.ontouchend to avoid more than one listener getting attached at a time to the button.
+      document.getElementById('commentSubmit').onclick = () => {
+        // debugger;
+        this.model.addComment(
+          postName,
+          document.getElementById('commentEntry').value
+        );
+        document.getElementById('commentEntry').value = '';
+        this.showCommentList(postName);
+      };
+    }
+    showCommentList(q = null) {
+      try {
+        const parent = document.getElementById(this.commentElementId);
+        if (!parent) throw new Error('comment parent not found');
+        // check to see if the commentUI code has been added yet
+        if (parent.innerHTML === '') {
+          parent.innerHTML = commentUI;
+        }
+        if (q !== null) {
+          // looking at one post, show comments and new comment button
+          document.querySelector('.addComment').style.display = 'block';
+          this.addSubmitListener(q);
+        } else {
+          // no post name provided, hide comment entry
+          document.querySelector('.addComment').style.display = 'none';
+        }
+        // get the comments from the model
+        let comments = this.model.getComments(q);
+        if (comments === null) {
+          // avoid an error if there are no comments yet.
+          comments = [];
+        }
+        renderCommentList(parent.lastChild, comments);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
+  
+  export default Comments;
