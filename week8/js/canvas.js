7f913bff37dc28505434759af58f55613335d42e
Kathy Moore <mookathy3@gmail.com>
2022-06-10 17:46:22 -0400

    commit

---
+++ week8/js/canvas.js
@@ -0,0 +1,15 @@
+//grab canvas element
+var canvas = document.getElementById("myCanvas");
+
+//We obtain our drawing context by calling the getContext method and passing it the string "2d", since we’ll be drawing in two dimensions
+var ctx = canvas.getContext("2d");
+
+// color strings
+ctx.strokeStyle = "red";
+ctx.fillStyle = "rgba(0, 0, 255, 0.5)";
+
+
+//to draw the rectangle
+//the fillRect and strokeRect methods. Both of these methods take the X and Y coordinates where you want to begin drawing the fill or the stroke, and the width and height of the rectangle.
+ctx.fillRect(10, 10, 100, 100);   
+ctx.strokeRect(10, 10, 100, 100); // the stroke and fill 10 pixels from the top and 10 pixels from the left of the canvas’s top-left corner
