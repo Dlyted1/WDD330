d444b609ff1c2ffc093c5db446144802abcf0bb8
Kathy Moore <mookathy3@gmail.com>
2022-06-10 22:44:10 -0400

    commit

--- week8/js/canvas.js
+++ week8/js/canvas.js
@@ -2,25 +2,33 @@
 var canvas = document.getElementById("myCanvas");
 
 //We obtain our drawing context by calling the getContext method and passing it the string "2d", since we’ll be drawing in two dimensions
-var ctx = canvas.getContext("2d");
+var context = canvas.getContext("2d");
 
 // color strings
-ctx.strokeStyle = "red";
-ctx.fillStyle = "rgba(0, 0, 255, 0.5)";
+context.strokeStyle = "red";
+context.fillStyle = "rgba(0, 0, 255, 0.5)";
 
 
 //to draw the rectangle
 //the fillRect and strokeRect methods. Both of these methods take the X and Y coordinates where you want to begin drawing the fill or the stroke, and the width and height of the rectangle.
-ctx.fillRect(10, 10, 100, 100);   
-ctx.strokeRect(10, 10, 100, 100); // the stroke and fill 10 pixels from the top and 10 pixels from the left of the canvas’s top-left corner
+context.fillRect(10, 10, 100, 100);   
+context.strokeRect(10, 10, 100, 100); // the stroke and fill 10 pixels from the top and 10 pixels from the left of the canvas’s top-left corner
 
 function drawPattern() {
     var canvas = document.getElementById("demo2");
-    var ctx = canvas.getContext("2d");
-    ctx.strokeStyle = "red";
+    var context = canvas.getContext("2d");
+    context.strokeStyle = "red";
+    console.log(drawPattern);
 
     var img = new Image(); 
     img.src = "images/oldbike.png";
-    img.onload = function() { 
-    };            
-}
+    img.onload = function() {
+            var pattern = context.createPattern(img, "repeat"); 
+            context.fillStyle = pattern;                        
+            context.fillRect(10, 10, 100, 100);                  
+            context.strokeRect(10, 10, 100, 100);             
+            };
+        }
+
+        
+   
