fe96cbcfc6a790f56d6b9d7f7baa78c6239a8f1e
Kathy Moore <mookathy3@gmail.com>
2022-06-15 11:55:00 -0400

    commit

--- week9/js/animation1.js
+++ week9/js/animation1.js
@@ -3,7 +3,9 @@
 const squareElement = document.getElementById('square');
 let angle = 0;
 
+
+//first argument to increase the value of angle by 2 then next argument equates to a frame speed of 60 frames per second
 setInterval( () => {
     angle = (angle + 2) % 360;
-    squareElement.style.transform = `rotate(${angle}deg)` //to increase the value of angle by 2
-}, 1000/60);   // 2nd argument equates to a frame speed of 60 frames per second
+    squareElement.style.transform = `rotate(${angle}deg)` 
+}, 1000/60);   
