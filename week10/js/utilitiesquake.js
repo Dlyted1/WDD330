a9449c9f0905c0feef7a925d083e8748088c96d2
Kathy Moore <mookathy3@gmail.com>
2022-06-22 13:41:39 -0400

    commit

--- week10/js/utilitiesquake.js
+++ week10/js/utilitiesquake.js
@@ -12,11 +12,6 @@ export function getJSON(url) {
         });
 }
         
-export const getLocation = function(options) {
-    return new Promise(function(resolve, reject) {
-        navigator.geolocation.getCurrentPosition(resolve, reject, options);
-    });
-}
 
 /*Below you will find a simple "Promisefied" version of the method to return the current location of the user.*/
 export const getLocation = function (options) {
