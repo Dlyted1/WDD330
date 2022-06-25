775928c24e346b2a4c1fafa103fc365afa95dbf5
Kathy Moore <mookathy3@gmail.com>
2022-06-24 20:00:57 -0400

    commit

--- week10/js/mainquake.js
+++ week10/js/mainquake.js
@@ -1,23 +1,13 @@
-import QuakesController from './controllerquake.js';
-import { getJSON, getLocation } from './utilitiesquake.js';
+// import QuakesController from './controllerquake.js';
+// import { getJSON, getLocation } from './utilitiesquake.js';
 
-const baseUrl = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2020-01-01&endtime=2020-02-02' +
-    `&latitude=-25.67` +
-    `&longitude=-80` +
-    `&maxradiuskm=500`
 
-const json = getJSON(baseUrl).then(data => console.log(data));
 
-getLocation().then(data => {
-    const latitude = data.coords.latitude;
-    const longitude = data.coords.longitude;
-    const position = {
-        lat: latitude,
-        lon: longitude
-    }
-    console.log(position);
-    const quakesController = new QuakesController("#quakeList", position);
-    console.log(quakesController);
-    quakesController.init();
-    quakesController.getQuakesByRadius(500);
-});
+    import QuakesController from './controllerquakes.js';
+    import buildNavigation from './routing.js';
+    
+    const navElement = document.getElementById('mainNav');
+    buildNavigation(navElement);
+    
+    // const myQuakesController = new QuakesController('#quakeList');
+    // myQuakesController.getQuakesByRadius();
