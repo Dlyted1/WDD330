3ff86555ca33bf0576c7b3c897155b6d323d0063
Kathy Moore <mookathy3@gmail.com>
2022-06-24 20:48:21 -0400

    commit

--- week10/js/mainquake.js
+++ week10/js/mainquake.js
@@ -1,12 +1,12 @@
 
 
-import { getJSON, getLocation } from './utilitiesquake.js';
+// import { getJSON, getLocation } from './utilitiesquake.js';
 import QuakesController from './controllerquake.js';
-import buildNavigation from './routing.js';
+// import buildNavigation from './routing.js';
  
 
-    const navElement = document.getElementById('mainNav');
-    buildNavigation(navElement);
+    // const navElement = document.getElementById('mainNav');
+    // buildNavigation(navElement);
     
     // const myQuakesController = new QuakesController('#quakeList');
     // myQuakesController.getQuakesByRadius();
