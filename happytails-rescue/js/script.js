96a43df76f4548629101ad88eff03d6e8e090d81
Kathy Moore <mookathy3@gmail.com>
2022-07-08 15:09:01 -0400

    commit

--- happytails-rescue/js/script.js
+++ happytails-rescue/js/script.js
@@ -1,1 +1,22 @@
-console.log(123);
+import {isValidZip, showAlert } from "./validate";
+
+const petForm = document.querySelector('#pet-form');
+
+petForm.addEventListener('submit', fetchAnimals);
+
+//fetch animals from API
+function fetchAnimals(e) {
+    e.preventDefault();
+
+    //get user input
+    const animal = document.querySelector('#animal').value;
+    const zip = document.querySelector('#zip').value;
+
+    //validate Zip
+    if (!isValidZip(zip)) {
+    showAlert("Please use a valid zipcode", "danger");
+    return;
+    }
+
+    //fetch animals or pets through api 
+
